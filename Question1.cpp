c3f91a4ed1a8ceb45e5c48e2604ea3f7194c6cf9
samkelo <sumcarelow@gmail.com>
2016-03-16 14:42:15 +0200

    commented line in question 1.1

--- Question1.cpp
+++ Question1.cpp
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
-// Name:
-// Student No:
-// Date: 
+// Name: samkelo zondi
+// Student No: 214538321
+// Date: 16-March-2016
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <string>
@@ -14,8 +14,8 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-	// 1.1 on branch "master", uncomment the following line and make a commi
-	cout << "This is the first line of code" << endl;
+	// 1.1 on branch "master", uncomment the following line and make a commit
+	//cout << "This is the first line of code" << endl;
 
 	// 1.2 create and switch to branch "hello", uncomment (and complete) the following line and make a commit  
 	// cout << "Hello, my name is ___________." << endl;
